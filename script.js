9e0ea53ae6bc8e643c7027643f521a487071a65a
sparky9043 <sparky9043@gmail.com>
2024-06-28 12:19:59 +0900

    Make start match function take row and column as arguments

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const GameConsole = () => {
   const columns = 3;
   const emptyMarker = null;
 
+  const players = [
+    {
+      name: 'playerOne',
+      marker: 'x',
+    },
+    {
+      name: 'playerTwo',
+      marker: 'o',
+    },
+  ];
+
+  let currentPlayer = players[0];
+  const updateCurrentPlayer = () => {
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+  }
+
   const board = [];
 
   for (let i = 0; i < rows; i++) {
@@ -14,8 +30,12 @@ const GameConsole = () => {
 
   const getBoard = () => board;
 
-  const startMatch = () => {
-    console.log('hello');
+  const startMatch = (row, column) => {
+    if (row < 0 || row > 2 || column < 0 || column > 2) return;
+    else if (board[row][column] !== null) return;
+    board[row][column] = currentPlayer.marker;
+    updateCurrentPlayer();
+    console.log(currentPlayer, board);
   }
 
   return {
