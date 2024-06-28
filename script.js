d25044796e64239de04774fb1cdd04df490078e6
sparky9043 <sparky9043@gmail.com>
2024-06-28 12:29:33 +0900

    Add logic to check win combination

--- script.js
+++ script.js
@@ -34,8 +34,30 @@ const GameConsole = () => {
     if (row < 0 || row > 2 || column < 0 || column > 2) return;
     else if (board[row][column] !== null) return;
     board[row][column] = currentPlayer.marker;
+    checkWinner(board);
     updateCurrentPlayer();
-    console.log(currentPlayer, board);
+  }
+
+  const checkWinner = (boardArray) => {
+    const flatBoard = boardArray.flat();
+    console.log(board);
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (flatBoard[a] && flatBoard[a] === flatBoard[b] && flatBoard[b] === flatBoard[c]) {
+        console.log('winner');
+      }
+    }
   }
 
   return {
