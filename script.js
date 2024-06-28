f34e8a241b592fd025646576c6bfb7b7db9bd528
sparky9043 <sparky9043@gmail.com>
2024-06-28 12:10:59 +0900

    Update generate board function to create rows and columns

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const GameConsole = () => {
   const rows = 3;
   const columns = 3;
+  const emptyMarker = null;
 
   const board = [];
 
   for (let i = 0; i < rows; i++) {
     board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(emptyMarker);
+    }
   }
 
   const getBoard = () => board;
